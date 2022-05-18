85e2f51ffc50861df71969097c24c022713d947c
SEHOON <devkwonsehoon@naver.com>
2022-05-18 19:59:28 +0900

    [INIT] 프로젝트 초기 세팅

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+import UserController from "./UserController";
+import ReviewController from "./ReviewController";
+import MovieController from "./MovieController";
+// controller index file
+export { UserController, ReviewController, MovieController };
---
+++ src/services/index.ts
@@ -0,0 +1,6 @@
+import UserService from "./UserService";
+import ReviewService from "./ReviewService";
+import MovieService from "./MovieService";
+
+//service index file
+export { UserService, ReviewService, MovieService };
